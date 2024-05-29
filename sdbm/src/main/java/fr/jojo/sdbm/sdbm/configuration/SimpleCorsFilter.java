6cc4e78be4670961ee14a1c6d61f7db119ef3453
DeusJojo <113697602+DeusJojo@users.noreply.github.com>
2024-05-29 19:36:08 +0200

    cors

---
+++ sdbm/src/main/java/fr/jojo/sdbm/sdbm/configuration/SimpleCorsFilter.java
@@ -0,0 +1,23 @@
+package fr.jojo.sdbm.sdbm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        String[] allowDomains = new String[2];
+        allowDomains[0] = "http://localhost:4200";
+        allowDomains[1] = "http://localhost:8090";
+
+        System.out.println("CORS configuration....");
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowDomains);
+            }
+        };
+    }
+}
