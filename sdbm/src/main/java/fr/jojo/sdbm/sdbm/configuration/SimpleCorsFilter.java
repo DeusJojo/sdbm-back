149d7935a225c5156dce88ef6a327b512d3aebd2
DeusJojo <113697602+DeusJojo@users.noreply.github.com>
2024-05-29 19:36:16 +0200

    cors

--- sdbm/src/main/java/fr/jojo/sdbm/sdbm/configuration/SimpleCorsFilter.java
+++ sdbm/src/main/java/fr/jojo/sdbm/sdbm/configuration/SimpleCorsFilter.java
@@ -1,23 +1,43 @@
-package fr.jojo.sdbm.sdbm.config;
+package fr.jojo.sdbm.sdbm.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        String[] allowDomains = new String[2];
-        allowDomains[0] = "http://localhost:4200";
-        allowDomains[1] = "http://localhost:8090";
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-        System.out.println("CORS configuration....");
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins(allowDomains);
-            }
-        };
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.LogRecord;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        Map<String, String> map = new HashMap<>();
+        String originHeader = request.getHeader("origin");
+        response.setHeader("Access-Control-Allow-Origin", originHeader);
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy(){}
 }
